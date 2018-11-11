93a277f174442ab8468c0c6bc1fb4be8b3b966c7
Eric Brown <eb@ericebrown.com>
2018-11-11 17:59:02 -0500

    Updated documentation, and moved non-user-facing loading functions to loading_utilities.R

--- R/loading.R
+++ R/loading.R
@@ -14,7 +14,12 @@
 # TAC files
 
 
-# Loads TAC information for use by other functions. Default is PMOD .tac format.
+#' Loads TAC from file for use by other functions. Default is PMOD .tac format.
+#'
+#'@param filename (e.g. participant.TAC)
+#'@param format (default, and only option currently, is .tac as from PMOD.
+#'@return data.frame with loaded TAC data
+#'@examples loadTACfile("/dir/participant1.tac")
 loadTACfile <- function(filename, format="PMOD") {
     if (format == "PMOD") {
         tac <- read.csv(filename, sep="")
@@ -25,74 +30,69 @@ loadTACfile <- function(filename, format="PMOD") {
 
 # ROI volume data
 
+#' Loads ROI volumes from file for use by other functions.
+#'
+#'@param filename (e.g. participant.voistat)
+#'@param format (default is the TAC .voistat format from PMOD).
+#'@return data.frame with loaded TAC data
+#'@examples loadVolumes("/dir/participant1_TAC.voistat")
 loadVolumes <- function(filename, format="Voistat", commontime="30") {
-    if (format == "Voistat") {
-        volumes <- volumesFromVoistatTAC(filename, commontime)
-    } else if (format == "BPndPaste") {
-        volumes <- volumesFromBPndPaste(filename)
-    } else stop("Specified format for volume data not supported.")
-}
-
-# BPnd data can be copied from PNEURO and saved as a CSV. It contains ROI volume
-# information. This extracts that.
-volumesFromBPndPaste <- function(BPnd_file) {
-    BPnd <- read.csv(BPnd_file, header=TRUE, row.names=1)
-    return(BPnd["Volume..ccm."])
-}
-
-# TAC .voistat files contain volume information for each ROI.
-# This extracts that. Common time is simply a frame time that appears once in
-# each ROI.
-volumesFromVoistatTAC <- function(voistat_file, commontime="30") {
-    voistat <- read.csv(voistat_file, sep="\t", skip=6, header=T,
-    stringsAsFactors=F)
-    # create a list of each unique ROI name
-    ROIs <- unique(voistat[,"VoiName.Region...string."])
-    # subset the voistat data to include just a single time frame, since
-    # volume is same for each time
-    u <- voistat$Time..seconds.==commontime
-    # create a list of the volumes for each ROI.
-    Volume..ccm. <- voistat[, "Volume..ccm."][u]
-    return(data.frame(ROIs, Volume..ccm., row.names=1))
+  if (format == "Voistat") {
+      volumes <- volumesFromVoistatTAC(filename, commontime)
+  } else if (format == "BPndPaste") {
+      volumes <- volumesFromBPndPaste(filename)
+  } else stop("Specified format for volume data not supported.")
+    
+  return(volumes)
 }
 
 
 # PET Model Data
 
-# .voistat files for each model (e.g Logan, not TAC) contain data for each
-# subROI, this extracts mean data for aggregate ROIs based on ROI_def.
+#' Loads model data from file for use by other functions.
+#'
+#'@param filename (e.g. participant_logan.voistat)  
+#'@param format (default is the TAC .voistat format from PMOD).
+#'@return data.frame with loaded model data in specified combined weighted ROIs.
+#'@examples loadVolumes("/dir/participant1_TAC.voistat")
 voistatScraper <- function(voistat_file, ROI_def=standardROIs(), model="VALUE") {
     
-    voistat <- read.csv(voistat_file, sep="\t", skip=6, header=T,
-                        stringsAsFactors=F)
-    ROIs <- voistat$VoiName.Region...string.
-    Volume..ccm. <- voistat$Volume..ccm.
-    Averaged..1.1. <- voistat$Averaged..1.1.
+  voistat <- read.csv(voistat_file, sep="\t", skip=6, header=T,
+                      stringsAsFactors=F)
+  ROIs <- voistat$VoiName.Region...string.
+  Volume..ccm. <- voistat$Volume..ccm.
+  Averaged..1.1. <- voistat$Averaged..1.1.
     
-    rawvolumes <- data.frame(ROIs, Volume..ccm., row.names=1)
-    storedvalues <- data.frame(ROIs, Averaged..1.1., row.names=1)
+  rawvolumes <- data.frame(ROIs, Volume..ccm., row.names=1)
+  storedvalues <- data.frame(ROIs, Averaged..1.1., row.names=1)
     
-    # Calculate the relative volumes for the subROIs and ROIs
-    proportiontable <- calcRelativeVolumes(rawvolumes, ROI_def)
+  # Calculate the relative volumes for the subROIs and ROIs
+  proportiontable <- calcRelativeVolumes(rawvolumes, ROI_def)
     
-    summarytable <- data.frame(
-    row.names = c(names(ROI_def@hemilobe), names(ROI_def@lobe), "totalcortical"),
-    VALUE = rep(0, length(c(names(ROI_def@hemilobe), names(ROI_def@lobe)))+1)
-    )
+  summarytable <- data.frame(
+  row.names = c(names(ROI_def@hemilobe), names(ROI_def@lobe), "totalcortical"),
+  VALUE = rep(0, length(c(names(ROI_def@hemilobe), names(ROI_def@lobe)))+1)
+  )
     
-    # Creates a mean table (data.frame to store the means and relative
-    # volumes of each ROI), and fills it.
-    means <- fill_table(mean_table(ROI_def), storedvalues, ROI_def,
-    proportiontable, headername="Averaged..1.1.")
+  # Creates a mean table (data.frame to store the means and relative
+  # volumes of each ROI), and fills it.
+  means <- fill_table(mean_table(ROI_def), storedvalues, ROI_def,
+  proportiontable, headername="Averaged..1.1.")
     
-    VALUEtable <- create_final_table(ROI_def, model)
+  VALUEtable <- create_final_table(ROI_def, model)
     
-    VALUEtable <- weighted_average(ROI_def@hemilobe, names(ROI_def@hemilobe),
-    means, VALUEtable, model, "proportion_of_hemilobe")
-    VALUEtable <- weighted_average(ROI_def@lobe, names(ROI_def@lobe), means,
-    VALUEtable, model, "proportion_of_lobe")
-    VALUEtable <- weighted_average(ROI_def@totalcortical, "totalcortical",
-    means, VALUEtable, model, "proportion_of_total")
+  VALUEtable <- weighted_average(ROI_def@hemilobe, names(ROI_def@hemilobe),
+  			     means, VALUEtable, model, "proportion_of_hemilobe")
+  VALUEtable <- weighted_average(ROI_def@lobe, names(ROI_def@lobe), means,
+  				                       VALUEtable, model, "proportion_of_lobe")
+  VALUEtable <- weighted_average(ROI_def@totalcortical, "totalcortical",
+                                means, VALUEtable, model, "proportion_of_total")
     
     return(VALUEtable)
 }
+
+
+
+
+
+
