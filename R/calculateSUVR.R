3a74220c5ee9f3460708efed9cea0e01a7d7a724
eebrown <eb@ericebrown.com>
2018-11-27 10:11:41 -0500

    Re-wrote calcSUVR to use weighted.mean() to simplify scripts.

--- R/calculateSUVR.R
+++ R/calculateSUVR.R
@@ -21,7 +21,7 @@
 #'@return A table of SUVR values for the specified ROIs
 #'@examples 
 #' calcSUVR(p1tac, p1vol, standardROIs(), c("3000", "3300", "3600", "3900"))
-calcSUVR <- function(tac, volumes, ROI_def, SUVR_def, corrected=TRUE) {
+legacy_calcSUVR <- function(tac, volumes, ROI_def, SUVR_def, corrected=TRUE) {
 
   if (FALSE == verify_window_durations(tac, SUVR_def)) {
     warning("Frames durations are of unequal length.")
@@ -64,4 +64,40 @@ calcSUVR <- function(tac, volumes, ROI_def, SUVR_def, corrected=TRUE) {
   SUVRtable <- SUVRtable/cerebellumreference
 
   return(SUVRtable)
+}
+
+#' Calculate weighted SUVRs for specified regions of interest
+#'
+#' When smaller ROIs need to be combined into larger ROIs, e.g. when TACs have
+#' been calculated for components of the frontal lobe, but an SUVR is desired
+#' for the entire frontal lobe, the TACs need to be combined, and the relative
+#' volumes of each needs to be taken into account. This function calculates
+#' those weighted means. It also takes into account potentially different
+#' frame durations.
+#'
+#'@param tac The time-activity curve data from loading function
+#'@param volumes The ROI volume data from loading function
+#'@param SUVR_def is a vector of the start times for window to be used in SUVR
+#'@return A data.frame of SUVR values for the specified ROIs
+#'@examples
+#' calcSUVR(p1tac, p1vol, standardROIs(), c("3000", "3300", "3600", "3900"))
+calcSUVR <- function(tac, volumes, ROI_def, SUVR_def, reference="cerebellum") {
+    
+    SUVR <- rep(NA, length(ROI_def))
+    SUVRtable <- data.frame(row.names=names(ROI_def), SUVR)
+    
+    # will need to validate that t1$start and t2$end are numeric
+    frames <- match(SUVR_def, tac$start)
+    frame_weights <- tac$end[frames] - tac$start[frames]
+    
+    weighted_tacs <- calcTAC(tac, volumes, ROI_def)
+    
+    for (ROI in names(ROI_def)) {
+        rich <- weighted.mean(weighted_tacs[frames,][,ROI], frame_weights)
+        poor <- weighted.mean(weighted_tacs[frames,][,reference], frame_weights)
+        SUVRtable[ROI, "SUVR"] <-  rich/poor
+    }
+    
+    return(SUVRtable)
+    
 }
