4ede2d532a3d954faef36a22705d30fbbced5aba
eebrown <eb@ericebrown.com>
2018-11-27 11:09:27 -0500

    Updated voistatScraper with weighted.mean() function.

--- R/loading.R
+++ R/loading.R
@@ -57,7 +57,7 @@ loadVolumes <- function(filename, format="Voistat", commontime="30") {
 #'@param format (default is the TAC .voistat format from PMOD).
 #'@return data.frame with loaded model data in specified combined weighted ROIs.
 #'@examples loadVolumes("/dir/participant1_TAC.voistat")
-voistatScraper <- function(voistat_file, ROI_def=standardROIs(), model="VALUE") {
+legacy_voistatScraper <- function(voistat_file, ROI_def=standardROIs(), model="VALUE") {
     
   voistat <- read.csv(voistat_file, sep="\t", skip=6, header=T,
                       stringsAsFactors=F)
@@ -93,8 +93,24 @@ voistatScraper <- function(voistat_file, ROI_def=standardROIs(), model="VALUE") 
     return(VALUEtable)
 }
 
-
-
-
-
-
+#' Loads model data from file for use by other functions.
+#'
+#'@param filename (e.g. participant_logan.voistat)
+#'@param format (default is the TAC .voistat format from PMOD).
+#'@return data.frame with loaded model data in specified combined weighted ROIs.
+#'@examples loadVolumes("/dir/participant1_TAC.voistat")
+voistatScraper <- function(voistat_file, ROI_def, model="VALUE") {
+    
+    voistat <- read.csv(voistat_file, sep="\t", skip=6, header=T, stringsAsFactors=F)
+    
+    VALUE <- rep(NA, length(ROI_def))
+    VALUEtable <- data.frame(row.names=names(ROI_def), VALUE)
+    
+    for (i in 1:length(ROI_def)) {
+        m <- match(ROI_def[[i]], voistat$VoiName.Region...string.)
+        VALUEtable[names(ROI_def)[i], "VALUE"] <- weighted.mean(voistat$Averaged..1.1.[m], voistat$Volume..ccm.[m])
+    }
+    
+    names(VALUEtable) <- model
+    return(VALUEtable)
+}
