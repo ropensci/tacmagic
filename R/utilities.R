b3fc0a648608f5d51aca70bb9afadd9cff40cb0d
eebrown <eb@ericebrown.com>
2018-11-30 10:23:50 -0500

    utilities.R no longer needed; can put specific utilities in relevant files.

--- R/utilities.R
+++
@@ -1,7 +0,0 @@
-##################################
-## PET Analysis in R            ##
-## utilities.R                  ##
-## (C) Eric E. Brown  2018      ##
-## PEAR v devel                 ##
-## Beta version--check all work ##
-##################################
