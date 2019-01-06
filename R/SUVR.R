67846072b71d988ed2fcb780fce8d81f1e4b8b63
eebrown <eb@ericebrown.com>
2019-01-06 10:19:17 -0500

    suvr_auc() as alternative to suvr() that uses AUC but should provide very similar results.

--- R/SUVR.R
+++ R/SUVR.R
@@ -7,12 +7,8 @@
 
 #' Calculate weighted SUVRs for specified regions of interest
 #'
-#' When smaller ROIs need to be combined into larger ROIs, e.g. when TACs have
-#' been calculated for components of the frontal lobe, but an SUVR is desired
-#' for the entire frontal lobe, the TACs need to be combined, and the relative
-#' volumes of each needs to be taken into account. This function calculates
-#' those weighted means. It also takes into account potentially different
-#' frame durations.
+#' Calculate the standardized uptake value ratio (SUVR) for all ROIs in the
+#' provided tac data, using the specified reference region.
 #'
 #'@export 
 #'@param tac The time-activity curve data from tac_roi()
@@ -35,3 +31,30 @@ suvr <- function(tac, SUVR_def, reference) {
     return(SUVRtable)
 }
 
+#' Calculate SUVRs for regions of interest with AUC from mid-frame times
+#'
+#' Calculate the standardized uptake value ratio (SUVR) for all ROIs in the
+#' provided tac data, using the specified reference region. This is an alternate
+#' to suvr() which should provide very similar values.
+#'
+#'@export 
+#'@param tac The time-activity curve data from tac_roi()
+#'@param SUVR_def a vector of start times for window to be used in SUVR
+#'@param reference is a string, e.g. "cerbellum", to specify reference region
+#'@return A data.frame of SUVR values for the specified ROIs
+suvr_auc <- function(tac, SUVR_def, reference) {
+
+    SUVRtable <- new_table(tac, "SUVR")
+    
+    tac$mid <- (tac$start + tac$end) / 2
+
+    for (ROI in names(tac)[-(1:2)]) {    
+        rich <- pracma::trapz(tac[(tac$start %in% SUVR_def),][,"mid"], 
+                              tac[(tac$start %in% SUVR_def),][,ROI])
+        poor <- pracma::trapz(tac[(tac$start %in% SUVR_def),][,"mid"], 
+                              tac[(tac$start %in% SUVR_def),][,reference])
+        SUVRtable[ROI, "SUVR"] <-  rich/poor
+    }    
+
+    return(SUVRtable)
+}
