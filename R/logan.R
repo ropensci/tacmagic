eb119c5fee783f854019115502d1ea886ea54cb1
eebrown <eb@ericebrown.com>
2019-01-08 21:28:55 -0500

    updated logan.R to add error argument for find_t_star()

--- R/logan.R
+++ R/logan.R
@@ -25,9 +25,11 @@
 #'@param t_star If 0, t* will be calculated using find_t_star()
 #'@param method Method of inntegration, "trapz" or "integrate"
 #'@return Data frame with calculate DVRs for all ROIs
-DVR_ref_Logan <- function(tac_data, target, ref, k2prime, t_star, 
+DVR_ref_Logan <- function(tac_data, target, ref, k2prime, t_star, error=0.10, 
                           method="trapz") {
-    model <- ref_Logan_lm(tac_data, target, ref, k2prime, t_star, method)
+    model <- ref_Logan_lm(tac_data=tac_data, target=target, ref=ref, 
+                          k2prime=k2prime, t_star=t_star, error=error, 
+                          method=method)
     DVR <- model$coefficients[[2]]
     return(DVR)
 }
@@ -45,16 +47,17 @@ DVR_ref_Logan <- function(tac_data, target, ref, k2prime, t_star,
 #'@param t_star If 0, t* will be calculated using find_t_star()
 #'@param method Method of inntegration, "trapz" or "integrate"
 #'@return Data frame with calculate DVRs for all ROIs
-DVR_all_ref_Logan <- function(tac_data, ref, k2prime, t_star, method="trapz") {
+DVR_all_ref_Logan <- function(tac_data, ref, k2prime, t_star, error=0.10, 
+                              method="trapz") {
     
     DVRtable <- new_table(tac_data, "DVR")
     
     ROIs <- names(tac_data)[3:length(names(tac_data))]
     for (ROI in ROIs) {
-        message(paste("Trying", ROI))
+        #message(paste("Trying", ROI))
         attempt <- try(DVR_ref_Logan(tac_data, target=ROI, ref=ref,
                                      k2prime=k2prime, t_star=t_star, 
-                                     method=method))
+                                     error=error, method=method))
         if (class(attempt) == "try-error") {
             attempt <- NA
         }
@@ -76,19 +79,22 @@ DVR_all_ref_Logan <- function(tac_data, ref, k2prime, t_star, method="trapz") {
 #'@param t_star If 0, t* will be calculated using find_t_star()
 #'@param method Method of inntegration, "trapz" or "integrate"
 #'@return No return
-plot_ref_Logan <- function(tac_data, target, ref, k2prime, t_star=0,
+plot_ref_Logan <- function(tac_data, target, ref, k2prime, t_star=0, error=0.1,
                            method="trapz") {
-    model <- ref_Logan_lm(tac_data, target, ref, k2prime, t_star, method)
-    xy <- ref_Logan_xy(tac_data, target, ref, k2prime, method)
+    model <- ref_Logan_lm(tac_data=tac_data, target=target, ref=ref, 
+                          k2prime=k2prime, t_star=t_star, error=error, 
+                          method=method)
+    xy <- ref_Logan_xy(tac=tac_data, target=target, ref=ref, 
+                       k2prime=k2prime, method=method)
     x <- xy$x
     y <- xy$y
-    if (t_star == 0) t_star <- find_t_star(x, y)
+    if (t_star == 0) t_star <- find_t_star(x, y, error=error)
     
     par(mfrow=c(1,2))
     
     plot_tac(tac_data, ROIs=c(target,ref))
     
-    plot(y~x)
+    plot(y~x, main=paste("Reference Logan model, reference =", ref))
     abline(model)
     abline(v=x[t_star])
 }
@@ -129,17 +135,19 @@ ref_Logan_xy <- function(tac, target, ref, k2prime, method) {
 
 # The non-invasive reference Logan method -- linear model starting from t*
 #' @noRd
-ref_Logan_lm <- function(tac_data, target, ref, k2prime, t_star, method) {
+ref_Logan_lm <- function(tac_data, target, ref, k2prime, t_star, error, method) {
     
-  xy <- ref_Logan_xy(tac_data, target=target, ref=ref, k2prime=k2prime, 
+  xy <- ref_Logan_xy(tac_data, target=target, ref=ref, k2prime=k2prime,
                      method=method)
   
   x <- xy$x
   y <- xy$y
   
-  if (t_star == 0) t_star <- find_t_star(x, y)
-  message(paste("t* is", t_star))
-  
+  if (t_star == 0) {
+    t_star <- find_t_star(x, y, error=error)
+    message(paste("t* is", t_star))
+  }
+
   model <- lm(y[t_star:length(y)] ~ x[t_star:length(x)])
   return(model)
 }
@@ -147,9 +155,9 @@ ref_Logan_lm <- function(tac_data, target, ref, k2prime, t_star, method) {
 
 # The Logan graphical analysis method finds the slope after a point t*; this
 # function calculates t* as the earliest time in which all residuals are less
-# than the specified proportion (default 0.10 or 10%) of the actual value.
+# than the specified proportion (default 0.1 or 10%) of the actual value.
 #' @noRd
-find_t_star <- function(x, y, error=0.10) {
+find_t_star <- function(x, y, error=0.1) {
     
     frames <- length(y)
     
